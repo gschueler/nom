4379f91f5bd8ad2b047bf768fc163d08a9def6ac
Greg Schueler <greg.schueler@gmail.com>
2012-02-15 20:47:11 -0800

    add explicit comments, text content, attributes

--- noml.groovy
+++ noml.groovy
@@ -21,7 +21,7 @@ public class DoNoml{
     
     public parseInput(input){
         def ctx=[]
-        def rparent=[subs:[],indent:-1,text:[]]
+        def rparent=[subs:[],indent:-1,text:[],attrs:[:]]
         def parent=rparent
         def last=parent
         input.eachLine{ line->
@@ -33,15 +33,30 @@ public class DoNoml{
             //current indent
             def rindent=(nindent-last.indent)
             
-            def m=(line=~/^(\w+)(\s+(.*))?$/)
-            def elem=[subs:[],indent:nindent,text:[]]
+            def m=(line=~/^(\w[\w:_\.-]+?)([:]?\s+(.*))?$/)
+            def mt=(line=~/^:(.*)?$/)
+            def mc=(line=~/^#(.*)?$/)
+            def ma=(line=~/^@(\w[\w:_\.-]+?)([:]?\s+(.*))$/)
+            def elem=[subs:[],indent:nindent,text:[],attrs:[:]]
             if(m.matches()){
                 elem.tag=m.group(1)
-                if(m.groupCount()>1){
-                    elem.attrs=m.group(3)
+                if(m.groupCount()>2 && m.group(3)){
+                    elem.attrtext=m.group(3)
                 }
+            }else if(mt.matches()){
+                last.text<<mt.group(1)
+                return
+            }else if(mc.matches()){
+                //comment
+                return
+            }else if(ma.matches()){
+                //attribute
+                if(ma.groupCount()>2){
+                    last.attrs[ma.group(1)]=ma.group(3)
+                }
+                return
             }else if(line.trim()){
-                parent.text<<line
+                last.text<<line
                 return
             }else{
                return
@@ -91,8 +106,8 @@ public class DoNoml{
     }
     public renderXml(xml,elem){
         def att=[:]
-        if(elem.attrs){
-            elem.attrs.split(/ /).each{a->
+        if(elem.attrtext){
+            elem.attrtext.split(/ /).each{a->
                 def b=a.split(/=/,2)
                 if(b.length>1 && b[0] && b[1]){
                     def val=b[1]
@@ -102,15 +117,16 @@ public class DoNoml{
                     att[b[0]]=val
                 }
             }
+            
             if(!att){
-                elem.text<<elem.attrs
+                elem.text.addAll(elem.attrtext)
             }
         }
         def text =''
         if(elem.text){
             text=elem.text.join("\n")
         }
-        xml."${elem.tag}"(att,text){
+        xml."${elem.tag}"(att+elem.attrs,text){
             if(elem.subs){
                 elem.subs.each{sub->
                     renderXml(delegate,sub)
